cdecfe818eb7d0086326c4665c9c8987e4406abb
Гузель <gguselia@mail.ru>
2022-12-06 17:20:41 +0500

    Создан модуль scores для вывод игровой информации

--- scores.py
+++ scores.py
@@ -2,3 +2,12 @@ import pygame.font
 
 
 class Scores():
+    def __init__(self, screen, stats):
+        self.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.stats = stats
+        self.text_color =(155, 195, 74)
+        self.font = pygame.font.SysFont(None, 36)
+        self.image_score()
+    def image_score(self):
+
