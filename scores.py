ad8004a8ff2160e459ae3156cb62c52983c0c37d
Гузель <gguselia@mail.ru>
2022-12-06 17:26:44 +0500

    Добавлена функция  image_score которая преобразовывает текст счета в графическое изображение

--- scores.py
+++ scores.py
@@ -9,5 +9,16 @@ class Scores():
         self.text_color =(155, 195, 74)
         self.font = pygame.font.SysFont(None, 36)
         self.image_score()
+
+
     def image_score(self):
+        self.score_img = self.font.render(str(self.stats.score), True, self.text_color, (0,0, 0))
+        self.score_rect = self.score_img.get_rect()
+        self.score_rect.right = self.screen_rect.right - 40
+        self.score_rect. top = 20
+
+
+
+
+
 
